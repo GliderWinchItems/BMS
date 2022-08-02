2dafc979e58579230fca1b9b27de3306de1bffb2
Donald <dhaselwood@verizon.net>
2022-08-02 09:22:36 -0400

    add missing BMSTask

---
+++ bmsadbms1818/Ourtasks/BMSTask.c
@@ -0,0 +1,149 @@
+/******************************************************************************
+* File Name          : BMSTask.c
+* Date First Issued  : 06/19/2022
+* Board              : bmsbms1818: STM32L431
+* Description        : Respond to queued read/write/etc requests
+*******************************************************************************/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "cmsis_os.h"
+#include "malloc.h"
+
+#include "BMSTask.h"
+#include "bmsspi.h"
+#include "morse.h"
+
+#include "main.h"
+#include "DTW_counter.h"
+#include "iir_f1.h"
+#include "bms_items.h"
+#include "rtcregs.h"
+
+struct BMSSPIALL bmsspiall;
+struct EXTRACTCONFIGREG extractconfigreg;
+struct EXTRACTSTATREG   extractstatreg;
+
+/* Queue */
+#define QUEUESIZE 16	// Total size of bcb's tasks can queue up
+osMessageQId BMSTaskReadReqQHandle;
+
+struct BMSREQ_Q* pssb; // Pointer to struct for request details
+
+TaskHandle_t BMSTaskHandle;
+
+uint32_t bmsdbctr; // Debug counter
+
+/* '1818 Wakeup keep-alive */
+#define WAKETICKS 1000 // 1 sec of FreeRTOS timer ticks
+TickType_t tickref;
+TickType_t tickwait;
+
+/* *************************************************************************
+ * void StartBMSTask(void const * argument);
+ *	@brief	: Task startup
+ * *************************************************************************/
+uint8_t dbgka;
+void StartBMSTask(void *argument)
+{
+	BaseType_t ret;
+
+	/* Execute a non-interrupt driven '1818 wake up sequence. */
+	bmsspi_wakeseq(); // Delay will be ~8 ms
+
+	/* Initialize configuration registers A & B. */
+	bmsspi_readstuff(READCONFIG);
+	bms_items_cfg_int(); // Initialize (memory) configreg bits
+	bmsspi_writereg(WRITECONFIG); // Write configreg to '1818'
+
+	tickref= xTaskGetTickCount();
+
+   	/* Infinite loop */
+  	for(;;)
+  	{
+  		/* Once per second with no slippage due to loop delay. */
+  		tickref += 500;//1000; //pdMS_TO_TICKS(1000);
+  		tickwait = tickref - xTaskGetTickCount();
+
+  		/* Check queue of loaded items. */
+		ret = xQueueReceive(BMSTaskReadReqQHandle,&pssb,tickwait);
+		if (ret == pdPASS)
+		{ // Request arrived
+			/* Execute request. */
+			switch (pssb->reqcode)
+			{
+			case REQ_BOGUS: // JIC debug
+				morse_trap(812);
+				break;
+			case REQ_READBMS:   // Read ADBMS1818
+				bmsspi_readbms();
+				break;
+//			case REQ_CALIB:    
+//				bmsspi_calib();
+//				break;
+//			case REQ_OPENCELL:// Do an open cell wire test
+//				bmsspi_opencell();
+//				break;
+//			case REQ_LOWPOWER:// Place into low power mode.
+//				bmsspi_lowpower();
+//				break;
+			case REQ_SETFETS: // Set discharge FETs
+				bmsspi_setfets();
+				break;
+			case REQ_TEMPERATURE:
+				bms_gettemp();
+				break;		
+			case REQ_RTCREAD:   // Read & check PEC15 on RTC registers
+				pssb->other = rtcregs_read();
+				break;		
+			case REQ_RTCUPDATE: // Update w PEC15 RTC registers
+				rtcregs_update();
+				break;		
+			default:
+				morse_trap(806); // Debugging trap
+				break;
+			}
+			/* Requesting task may not wait, but polls 'done' to signal completion. */
+			pssb->done = 0; // Show request completed
+
+			/* Requesting task may be waiting and needs to be notified. */
+			if (pssb->noteyes != 0)
+			{
+				ret = xTaskNotify(pssb->bmsTaskHandle, pssb->tasknote, eSetBits);
+ 				if (ret != pdPASS) morse_trap(247);
+ 			}
+	  	}
+	  	else
+	  	{ // Timeout waiting for queued request. Execute a keep-awake command. */
+// Debug: dbgka used by main.c to clocking display	  		
+	  		dbgka = bmsspi_keepawake();
+// Running count, debugging
+bmsdbctr += 1;	  	
+	  	}
+	}
+}
+
+/* *************************************************************************
+ * osThreadId xBMSTaskCreate(uint32_t taskpriority);
+ * @brief	: Create task; task handle created is global for all to enjoy!
+ * @param	: taskpriority = Task priority (just as it says!)
+ * @return	: BMSTaskHandle
+ * *************************************************************************/
+osThreadId xBMSTaskCreate(uint32_t taskpriority)
+{
+	BaseType_t xRet;
+
+	xRet = xTaskCreate(
+		StartBMSTask,     /* Function that implements the task. */
+		"BMSTask",        /* Text name for the task. */
+		256,              /* Stack size in words, not bytes. */
+		NULL,             /* Parameter passed into the task. */
+		taskpriority,     /* Priority at which the task is created. */
+		&BMSTaskHandle ); /* Used to pass out the created task's handle. */ 
+
+	if( xRet != pdPASS )return NULL;
+
+	BMSTaskReadReqQHandle = xQueueCreate(QUEUESIZE, sizeof(struct BMSREQ_Q*) );
+	if (BMSTaskReadReqQHandle == NULL) return NULL;
+
+   	return BMSTaskHandle;	
+}
---
+++ bmsadbms1818/Ourtasks/BMSTask.h
@@ -0,0 +1,105 @@
+/******************************************************************************
+* File Name          : BMSTask.h
+* Date First Issued  : 06/19/2022
+* Board              : bmsbms1818: STM32L431
+* Description        : Respond to queued read/write/etc requests
+*******************************************************************************/
+#ifndef __BMSTASK
+#define __BMSTASK
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "cmsis_os.h"
+#include "stm32l4xx_hal.h"
+#include "adcparams.h"
+#include "main.h"
+#include "bq_items.h"
+
+#define BMSTSKNOTEBIT00 (1<<0)
+
+/* SPI 12 bytes: CMD[2]+PEC[2]+DATA[6]+PEC[2] */
+// Note: uint16_t are sent/received big endian
+union SPI12
+{
+	uint8_t   u8[12];
+	uint16_t u16[6];
+	uint32_t u32[3];
+};
+
+/* Request Codes for services. */
+#define REQ_BOGUS    0  // This is a bogus request code trap
+#define REQ_READBMS  1	// Read ADBMS1818 cells + GPIO1,2
+#define REQ_CALIB    2  // Execute a self-calib (MAX14921 Aout offset) cycle
+#define REQ_OPENCELL 3  // Do an open cell wire test
+#define REQ_LOWPOWER 4  // Set low power mode.
+#define REQ_SETFETS  5  // Set cell discharge FETs 
+#define REQ_TEMPERATURE 6  // Read & calibrate GPIO temperature sensors
+#define REQ_RTCREAD     7  // Read & check PEC15 on RTC registers
+#define REQ_RTCUPDATE   8  // Update w PEC15 RTC registers
+
+struct EXTRACTSTATREG
+{
+	float     sc;   // Sum of Cells (volts)
+	float   itmp;   // Internal die temperature (deg C)
+	float     va;   // Analog supply voltage (volts)
+	float     vd;   // Digital supply (volts)
+	uint32_t cov;   // Overvoltage bits 
+	uint32_t cuv;   // Undervoltage bits
+	uint8_t  rev;   // Revision code
+	uint8_t  muxfail; 
+	uint8_t  thsd;  // 1 = thermal shutdown 
+};
+
+struct EXTRACTCONFIGREG
+{  // extract settings
+	float    vov;   // Readback of overvoltage setting
+	float    vuv;   // Readback of undervoltage setting
+	uint32_t dcc;   // Discharge cell bits: 1 = ON
+	uint16_t gpio;  // 9 GPIO's: 0 = pin at logic 0;
+	uint16_t cfbr1; // bits: MUTE,FDRF,PS1,PS0,DTMEN,DCC0,0,0
+};
+
+/* Struct (mostly) holds register group readings from '1818 */
+// Note: 'reg arrays are converted to little endian
+struct BMSSPIALL
+{
+	uint16_t configreg[2*3]; // Configuration regs: A B
+	uint16_t cellreg[6*3]; // Cell volt regs: A B C D E F
+	uint16_t auxreg [4*3]; // Aux regs: A B C D
+	uint16_t statreg[2*3]; // Status regs: A B
+	uint16_t commreg[1*3]; // COMM register
+	uint16_t sreg[1*3];    // S register
+	uint16_t pwmreg[2*3];  // PWM regs: A & B
+	uint8_t  err;          // TODO?
+};
+/* Queue holds pointers to the following struct for requesting a "service". */
+struct BMSREQ_Q
+{
+	TaskHandle_t bmsTaskHandle; // Requesting task's handle
+	BaseType_t  tasknote; // Requesting task's notification bit
+	uint32_t setfets;     // Discharge FET settings
+	uint8_t done;         // 0 = done; 1 = busy;
+	uint8_t noteyes;      // 1 = Notify requesting task when complete
+	uint8_t reqcode;      // Code for specific service requested
+	int8_t other;        // BMSTask sends back something to requester.
+};	
+
+/* *************************************************************************/
+osThreadId xBMSTaskCreate(uint32_t taskpriority);
+/* @brief	: Create task; task handle created is global for all to enjoy!
+ * @param	: taskpriority = Task priority (just as it says!)
+ * @return	: BMSTaskHandle
+ * *************************************************************************/
+
+extern TaskHandle_t BMSTaskHandle;// replace: extern osThreadId BMSTaskHandle;
+
+extern osMessageQId BMSTaskReadReqQHandle;
+
+extern struct BMSSPIALL bmsspiall;
+extern struct EXTRACTSTATREG extractstatreg;
+extern struct EXTRACTCONFIGREG extractconfigreg;
+
+extern struct BMSREQ_Q* pssb; // Pointer to struct for request details
+
+
+#endif
