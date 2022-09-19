98a8444701e4ae6e78e3fe0060d4dd152b130a22
Donald <dhaselwood@verizon.net>
2022-09-18 23:23:28 -0400

    save and backup work

--- bmsadbms1818/startup_stm32l431xx.s
+++ bmsadbms1818/startup_stm32l431xx.s
@@ -59,6 +59,7 @@ defined in linker script */
     .section	.text.Reset_Handler
 	.weak	Reset_Handler
 	.type	Reset_Handler, %function
+	.word	/*I_AM_CANID*/
 	.word	_begin_flash
 	.word   _end_prog
 Reset_Handler:
