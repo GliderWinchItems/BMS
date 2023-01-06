28b455abe17378130e2e6c327edab930b496fa8f
Donald <dhaselwood@verizon.net>
2023-01-05 23:15:36 -0500

    B0E00000 params mods

--- bmsadbms1818/params/B0E00000-bq_idx_v_struct.c
+++ bmsadbms1818/params/B0E00000-bq_idx_v_struct.c
@@ -49,10 +49,10 @@ void bq_idx_v_struct_hardcode_params(struct BQLC* p)
    p->tim1_ccr1_on_vlc =    2; // PWM ON count: Very Low Charge rate required
    p->tim1_arr_init    =   79; // At 16 MHz: count of 80 = 5 us PWM frame
 
-   p->cellv_max   = 3450; // Max limit (mv) for charging any cell
+   p->cellv_max   = 3480; // Max limit (mv) for charging any cell
    p->cellv_min   = 2200; // Min limit (mv) for any discharging
    p->cellv_vlc   = 2100; // Below this (mv) Very Low Charge (vlc)required
-   p->cellv_tgtdelta = 20; // Target delta (mv)
+   p->cellv_tgtdelta = 10; // Target delta (mv)
    p->cellopen_hi = 4000; // Above this voltage cell wire is assumed open (mv)
    p->cellopen_lo =  333; // Below this voltage cell wire is assumed open (mv)
    p->modulev_max = (16*3600); // Battery module max limit (mv)
